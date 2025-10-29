b373919b808922879bf661b1a35f776199289ff8
Matis Martin <matis.martin@outlook.fr>
2025-10-29 16:13:40 +0100

    app fonctionnelle

--- script.js
+++ script.js
@@ -1,20 +1,101 @@
-const btn = document.getElementById("btn");
+const randomBtn = document.getElementById("random");
+const allBtn = document.getElementById("all");
+const addBtn = document.getElementById("add");
 const question = document.getElementById("question");
 const answer = document.getElementById("answer");
+const sections = document.querySelectorAll("section");
+const blaguesList = document.getElementById("blagues-list");
+const addForm = document.getElementById("add-form");
+const message = document.getElementById("add-message");
 
-const API_URL = "http://localhost:5000/api/v1/blagues/random";
+const API_URL_RANDOM = "http://localhost:5000/api/v1/blagues/random";
+const API_URL_ALL = "http://localhost:5000/api/v1/blagues";
+const API_URL_ADD = "http://localhost:5000/api/v1/blagues";
 
-async function getBlague() {
+function showSection(sectionId) {
+  document;
+  sections.forEach((section) => section.classList.add("hidden"));
+  document.getElementById(sectionId).classList.remove("hidden");
+}
+
+// Navigation
+randomBtn.addEventListener("click", () => showSection("random-section"));
+allBtn.addEventListener("click", () => {
+  showSection("all-section");
+  getAllBlagues();
+});
+addBtn.addEventListener("click", () => showSection("add-section"));
+
+// RANDOM
+
+async function getRandomBlague() {
   try {
-    const response = await fetch(API_URL);
+    const response = await fetch(API_URL_RANDOM);
     const data = await response.json();
     question.textContent = data.question;
-    reponse.textContent = data.reponse;
+    answer.textContent = data.reponse;
   } catch (err) {
     question.textContent = "Erreur";
-    reponse.textContent = "";
+    answer.textContent = "";
     console.error(err);
   }
 }
 
-btn.addEventListener("click", getBlague);
+randomBtn.addEventListener("click", getRandomBlague);
+
+// ALL
+
+async function getAllBlagues() {
+  try {
+    blaguesList.innerHTML = "";
+    const response = await fetch(API_URL_ALL);
+    const data = await response.json();
+
+    data.forEach((blague) => {
+      const liQuestion = document.createElement("li")
+      liQuestion.classList.add('question');
+      const liAnswer = document.createElement("li");
+      liAnswer.classList.add('answer');
+      liQuestion.textContent = `${blague.question}`;
+      liAnswer.textContent = `${blague.reponse}`;
+      blaguesList.appendChild(liQuestion);
+      blaguesList.appendChild(liAnswer);
+    });
+  } catch (err) {
+    question.textContent = "Erreur";
+    answer.textContent = "";
+    console.error(err);
+  }
+}
+
+allBtn.addEventListener("click", getAllBlagues);
+
+
+
+addForm.addEventListener("submit", async (e) => {
+  e.preventDefault(); // empêche le rechargement de la page
+
+  const questionValue = document.getElementById("new-question").value;
+  const reponseValue = document.getElementById("new-reponse").value;
+
+  try {
+    const response = await fetch(API_URL_ADD, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        question: questionValue,
+        reponse: reponseValue
+      })
+    });
+
+    if (response.ok) {
+      message.textContent = "Blague ajoutée avec succès !";
+      addForm.reset();
+    } else {
+      message.textContent = "Erreur lors de l'ajout.";
+    }
+  } catch (err) {
+    console.error("Erreur :", err);
+    message.textContent = "Erreur de connexion au serveur.";
+  }
+});
